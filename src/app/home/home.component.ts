3e19352308682cf8364cd3080da80e9b2757ce5c
Goksen <goksengorgulu@gmail.com>
2020-07-26 22:14:59 +0200

    receive and display name property at home page (two-way data binding)

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -5,8 +5,13 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
+
+// clickCounter one way data binding
+
+//name two way data binding
 export class HomeComponent implements OnInit {
   clickCounter: number = 0;
+  name: string = '';
 
   constructor() {}
 
