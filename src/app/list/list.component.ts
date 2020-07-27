95f3e95d3119e99ca731e008f2b30d5a50b00b74
Goksen <goksengorgulu@gmail.com>
2020-07-27 10:59:36 +0200

    get breweries data with HttpClient

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -7,9 +7,16 @@ import { HttpService } from '../http.service';
   styleUrls: ['./list.component.scss']
 })
 export class ListComponent implements OnInit {
+  //define type of brews
+  brews: Object;
+
   constructor(private _http: HttpService) {}
 
   ngOnInit(): void {
     this._http.myMethod();
+    this._http.getBeer().subscribe(data => {
+      this.brews = data;
+      console.log('HERE IS THE BREWS', data);
+    });
   }
 }
