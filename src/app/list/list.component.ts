6ac12248b9f4ceb669f9218ba278e909078e2748
Goksen <goksengorgulu@gmail.com>
2020-07-27 10:34:28 +0200

    create http service

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpService } from '../http.service';
 
 @Component({
   selector: 'app-list',
@@ -6,10 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./list.component.scss']
 })
 export class ListComponent implements OnInit {
-
-  constructor() { }
+  constructor(private _http: HttpService) {}
 
   ngOnInit(): void {
+    this._http.myMethod();
   }
-
 }
